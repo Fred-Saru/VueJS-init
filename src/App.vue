6bc84e8cbb339d95a70ecab7be1b2bd6be20bbc2
Fred <alcide.fred@gmail.com>
2018-08-15 16:42:08 +0200

    End of assignement 8. Slot and dynamic component loading.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,38 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>
         <div class="row">
-            <servers></servers>
-            <app-server-details></app-server-details>
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" v-on:click="selectedTemplate = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" v-on:click="selectedTemplate = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" v-on:click="selectedTemplate = 'appRed'">Load Red Template</button>
+                <hr>
+                <component v-bind:is="selectedTemplate">
+                  <h3>I am the Blue zone !</h3>
+                </component>  
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-import Header from "./components/shared/Header";
-import Servers from "./components/server/Servers";
-import ServerDetails from "./components/server/ServerDetails";
-import Footer from "./components/shared/Footer";
+import Blue from "./components/assignment_8/Blue.vue";
+import Green from "./components/assignment_8/Green.vue";
+import Red from "./components/assignment_8/Red.vue";
 
 export default {
   components: {
-    "app-header": Header,
-    servers: Servers,
-    "app-server-details": ServerDetails,
-    "app-footer": Footer
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  },
+  data() {
+    return {
+      selectedTemplate: "appBlue"
+    };
   }
 };
 </script>
 
 <style>
-div.component {
-  border: 1px solid black;
-  padding: 30px;
-}
 </style>
