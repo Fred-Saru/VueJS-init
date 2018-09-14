d1ea68683a6b10b0b91dbc28086a62cb59928856
ALCIDE Frederic - CGI <frederic.alcide@rd.loreal.com>
2018-09-14 14:41:53 +0200

    End of introduction to vue-resource. Lib use to custom http request in your front app.

--- src/App.vue
+++ src/App.vue
@@ -2,76 +2,89 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 class="text-center">The Super Quizz</h1>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <transition name="flip" mode="out-in">
-                    <component :is="mode" v-on:answered="answered($event)" v-on:confirmed="confirmed"></component>
-                </transition>
+                <h1 class="text-center">HTTP</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label>Mail</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" v-on:click="submit">Submit</button>
+                <hr>
+                <input class="form-control" type="text" v-model="node">
+                <br><br>
+                <button class="btn btn-primary" v-on:click="fetchData">Get data</button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="(u, index) in users" v-bind:key="index">
+                        {{u.username}}: {{u.email}}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Question from './components/Question';
-    import Answer from './components/Answer';
 
     export default {
-        components: {
-            'app-question': Question,
-            'app-answer': Answer,
-        },
         data() {
             return {
-                mode: 'app-question'
+                user: {
+                    username: '',
+                    email: ''
+                },
+                users:[],
+                resource: {},
+                node: 'data'
             }
         },
         methods: {
-            answered(isCorrect) {
-                if(isCorrect) {
-                    this.mode = 'app-answer';
-                } else {
-                    this.mode = 'app-question';
+            submit() {
+                if(!this.user.username || !this.user.email) {
+                    return;
                 }
+
+                // this.$http.post('data.json', this.user)
+                // .then((response) => {
+                //     console.log(response);
+                // })
+                // .catch((error) => {
+                //     console.error(error);
+                // });
+
+                this.resource.save({node: this.node}, this.user);
+
+                //this.resource.saveAlt(this.user);
             },
-            confirmed() {
-                this.mode = 'app-question';
+            fetchData() {
+                this.resource.getData({node: this.node})
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) => {
+                        this.users = Object.keys(data).reduce((acc, key) => {
+                            acc.push(data[key]);
+                            return acc;
+                        }, []);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
             }
+        },
+        created() {
+            const customAction = {
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
+            };
+
+            this.resource = this.$resource('{node}.json', {}, customAction);
         }
     }
 </script>
 
 <style>
-.flip-enter-active {
-    animation: flip-in 0.8s ease-out forwards;
-}
-
-.flip-leave-active {
-    animation: flip-out 0.8s ease-out forwards;
-}
-
-@keyframes flip-out {
-    from {
-        transform: rotateY(0deg);
-    }
-    to  {
-        transform: rotateY(90deg);
-    }
-}
-
-@keyframes flip-in {
-    from {
-        transform: rotateY(90deg);
-    }
-    to  {
-        transform: rotateY(0deg);
-    }
-}
-
-
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from "vue";
+import VueResource from 'vue-resource';
 import App from "./App.vue";
+
+
+
+Vue.use(VueResource);
+Vue.http.options.root = "https://vue-http-f31be.firebaseio.com/";
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  next(response => {
+    console.log(response);
+  });
+});
 
 new Vue({
   el: "#app",
